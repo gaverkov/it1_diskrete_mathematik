1b7951bb4e1a0632c834b99f1a850cfda5688c00
Gennadiy Averkov <gennadiyaverkov@Gennadiys-MBP.fritz.box>
2021-11-11 06:07:14 +0100

    autogenerated makefile

---
+++ Latex/tex_makefile_creator.py
@@ -0,0 +1,46 @@
+import os, re
+
+def is_texfile(filename):
+    return os.path.isfile(filename) and \
+    len(filename)>3 and \
+    filename[-3:].lower()=='tex'
+
+def is_main(filename):
+    if not is_texfile(filename):
+        return False
+    with open(filename,'r') as f:
+        contents=f.read()
+        if r'\begin{document}' in contents:
+            return True
+        return False
+
+def inclusions(filename):
+    if not is_texfile(filename):
+        return []
+    with open(filename,'r') as f:
+        contents=f.read()
+        return(re.findall(r"\\(?:input|lstinputlisting|includegraphics)\{([^\}]+)",contents))
+
+
+print("all : ",end=" ")
+
+for filename in sorted(os.listdir()):
+    if is_main(filename):
+        print(filename[:-3]+"pdf",end=" ")
+
+print()
+
+for filename in sorted(os.listdir()):
+    if is_texfile(filename):
+        if is_main(filename):
+            print(filename[:-3]+"pdf : "+filename,end=" ")
+            for inc in Inclusions:
+                print(inc,end=" ")
+            print("\n\tpdflatex",filename)
+            print("\tpdflatex",filename)
+        else:
+            Inclusions=inclusions(filename)
+            if len(Inclusions)>0:
+                print(filename," : "," ".join(Inclusions))
+
+print(".PHONY : all")
